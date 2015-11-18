579fd3d81040a649d1b4a7565382f471d9549948
PretentiousGeek <cai.revell@gmail.com>
2015-11-18 19:56:25 +0000

    Blurgh

---
+++ City Builder/Assets/Scripts/SpawnBuilding.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SpawnBuilding : MonoBehaviour {
+
+	public Transform inst;
+	GameObject build;
+	bool building;
+
+	void Update(){
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if(Input.GetMouseButtonDown(0)){
+			building = true;
+		}
+
+		if(Input.GetMouseButtonUp(0)){
+			if(Physics.Raycast(ray, out hit)){
+				Instantiate(inst, new Vector3(Mathf.Round(hit.point.x), 0, Mathf.Round(hit.point.z)), Quaternion.identity);
+			}
+		}
+
+		if(Input.GetMouseButton(0)){
+			if(building){
+				if(Physics.Raycast (ray, out hit)){
+					Destroy (GameObject.Find ("DESTROY ME"));
+					build = (GameObject)Instantiate(inst, new Vector3(Mathf.Round(hit.point.x), 0, Mathf.Round(hit.point.z)), Quaternion.identity);
+					build.name = "DESTROY ME";
+				}
+			}
+		}
+
+	}
+
+}
